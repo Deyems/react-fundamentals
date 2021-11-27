1e09f37baba4003313400b049db11654d4392180
deyems <adeyemisola11@gmail.com>
2021-11-27 09:04:53 +0100

    working with combining styles in a component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,9 +12,18 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+const Box = ({size, className, style, ...otherProps}) => {
+  console.log(`style`, style)
+  console.log(`otherProps`, otherProps)
+  return (
+    <div className={`box ${size}`} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+    // <div className={`box ${size}`} style={style} {...otherProps} />
+  );
+}
+
+const smallBox = <Box size="small" className="box--small" style={{backgroundColor: 'lightblue'}}>Small light blue</Box>
+const mediumBox = <Box size="medium" className="box--medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
+const largeBox = <Box size="large" className="box--large" style={{backgroundColor: 'orange'}}>large orange box</Box>
 
 function App() {
   return (
